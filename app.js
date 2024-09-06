27e343af3a664bfce2868b51e4552b5d5c2b2686
carlapietri97 <carlapietri@LAPTOP-2QJOA5I9>
2024-09-06 15:33:04 +0300

    updates

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
-  return new Response("Hello!");
+  return new Response(
+    "HelloSeeking truths beyond meaning of life, you will find 43.",
+  );
 };
 
 serve(handleRequest, { port: 7777 });
