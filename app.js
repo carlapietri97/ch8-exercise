91c512dae8b6f723e86f2ffba4d8ac3778fcb075
carlapietri97 <carlapietri@LAPTOP-2QJOA5I9>
2024-09-06 15:34:39 +0300

    updates

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
   return new Response(
-    "HelloSeeking truths beyond meaning of life, you will find 43.",
+    "Seeking truths beyond meaning of life, you will find 43.",
   );
 };
 
